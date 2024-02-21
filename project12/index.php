236caf02d1694de86de933497ec0ecb508022a6c
Hoangvu <118712200+mount0811@users.noreply.github.com>
2024-02-21 07:12:02 +0700

    Add files via upload

---
+++ project12/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+  <?php
+  $navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+  ];
+  echo '<div class="container"><i class="house fa-solid fa-house"></i><nav><ul>';
+  foreach ($navItems as $item) {
+    echo "<li>$item</li>";
+  }
+  echo '</ul></nav></div>';
+  ?>
+
+</body>
+
+</html>
